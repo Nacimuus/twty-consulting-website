d284ffcb036689385ab3e4dbd2d1436f7af3ff67
Macintosh <macintosh@Macintoshs-MacBook-Pro.local>
2025-07-21 21:07:41 +0100

    head

---
+++ include.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Load header
+    fetch("header.html")
+      .then(res => res.text())
+      .then(data => {
+        document.getElementById("header").innerHTML = data;
+      });
+  
+    // Load footer
+    fetch("footer.html")
+      .then(res => res.text())
+      .then(data => {
+        document.getElementById("footer").innerHTML = data;
+      });
+  });
+  
